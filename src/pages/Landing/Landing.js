9f8f0f3cde9b110565473eff4b7fd9f11736bceb
Arinc Firincioglu <arenchf@gmail.com>
2022-08-08 21:58:39 +0200

    landing-page init

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -4,13 +4,15 @@ import Footer from '../../components/Footer/Footer'
 import './Landing.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faPenToSquare,faBook,faInfo} from '@fortawesome/free-solid-svg-icons';
+import {useNavigate} from 'react-router-dom'
 
 function Landing() {
+  const navigate = useNavigate()
   return (
     <div className='landing-page'>
         <Header></Header>
         <div className='content'>
-            <div className='title'>Welcome to Vobolio, where you can store your own dictionaries!</div>
+            <div className='title'><h2>Welcome to <b>Vobolio</b>, where you can store your own dictionaries!</h2></div>
             <div className='icon-cards-row'>
               <div className='icon-card'><div className='icon-wrapper'><FontAwesomeIcon icon={faBook}></FontAwesomeIcon></div>
               <div className='icon-card-label'>Create</div>
@@ -24,8 +26,8 @@ function Landing() {
                 </div>
             </div>
             <div className='user-row'>
-              <div className='sign-up-button'><button>Sign Up For Free</button></div>
-              <div className='login-button'><button>Login</button></div>
+              <div className='sign-up-button'><button onClick={()=>{navigate("/register")}}>Sign Up For Free</button></div>
+              <div className='login-button'><button onClick={()=>{navigate("/login")}}>Login</button></div>
             </div>
 
 
