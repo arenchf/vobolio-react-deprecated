293585b5745cdbc4b0d3d6500879bf37c689b69f
Arinc Firincioglu <arenchf@gmail.com>
2022-08-08 20:56:32 +0200

    changed ip to production

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// const baseURL = 'https://api.vobolio.com/api/'
-const baseURL = 'http://127.0.0.1:8000/api/'
+const baseURL = 'https://api.vobolio.com/api/'
+// const baseURL = 'http://127.0.0.1:8000/api/'
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
